20b8cadd55c519af8573726e97b69004b111a850
Pablo <pablo.magarinos@outlook.com>
2023-08-18 12:06:50 +0200

    Change of the function names
    Change the names of the augmentation functions inside augmentator.py

--- augmentator.py
+++ augmentator.py
@@ -3,29 +3,29 @@ import cv2
 import numpy as np
 import random
 
-def aug_exposure(image, maxfactor):
+def exp(image, maxfactor):
     factor = random.uniform(1-(maxfactor-1), maxfactor)
     return np.clip(image * factor, 0, 255).astype(np.uint8)
 
-def aug_sat(image, maxfactor):
+def sat(image, maxfactor):
     factor = random.uniform(1-(maxfactor-1), maxfactor)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     hsv[:,:, 1] = np.clip(hsv[:,:, 1] * factor, 0, 255)
     return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-def aug_hue(image, maxangle):
+def hue(image, maxangle):
     angle = random.uniform(-maxangle, maxangle)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     hsv[:,:, 0] = np.clip(hsv[:,:, 0] + angle, 0, 180)
     return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-def aug_xflip(image, prob):
+def xflip(image, prob):
     if random.random()<prob:
         return cv2.flip(image,0)
     else:
         return image
     
-def aug_yflip(image, prob):
+def yflip(image, prob):
     if random.random()<prob:
         return cv2.flip(image,1)
     else:
@@ -38,7 +38,7 @@ def getlabel(label):
     return label_content
 
 
-def savephoto(image_path, label_path, name, final_path, augments=2, exposure=True, exposure_maxfactor=1.4, saturation=True, saturation_maxfactor=1.4, hue=True, hue_maxangle=10, xflip = True, xflip_prob = 0.5, yflip = True, yflip_prob = 0.5):
+def savephoto(image_path, label_path, name, final_path, augments=2, do_exposure=True, exposure_maxfactor=1.4, do_saturation=True, saturation_maxfactor=1.4, do_hue=True, hue_maxangle=10, do_xflip = True, xflip_prob = 0.5, do_yflip = True, yflip_prob = 0.5):
     image = cv2.imread(image_path)
     try:
         label = open(label_path, 'r')
@@ -64,16 +64,16 @@ def savephoto(image_path, label_path, name, final_path, augments=2, exposure=Tru
     for i in range(augments):
         image_mod = image.copy()
 
-        if exposure:
-            image_mod = aug_exposure(image_mod, exposure_maxfactor)
-        if saturation:
-            image_mod = aug_sat(image_mod, saturation_maxfactor)
-        if hue:
-            image_mod = aug_hue(image_mod, hue_maxangle)
-        if xflip:
-            image_mod = aug_xflip(image_mod, xflip_prob)
-        if yflip:
-            image_mod = aug_yflip(image_mod, yflip_prob)
+        if do_exposure:
+            image_mod = exp(image_mod, exposure_maxfactor)
+        if do_saturation:
+            image_mod = sat(image_mod, saturation_maxfactor)
+        if do_hue:
+            image_mod = hue(image_mod, hue_maxangle)
+        if do_xflip:
+            image_mod = xflip(image_mod, xflip_prob)
+        if do_yflip:
+            image_mod = yflip(image_mod, yflip_prob)
 
         #Now that we have the modified image, we save it
         os.chdir(f"{final_path}/images")
